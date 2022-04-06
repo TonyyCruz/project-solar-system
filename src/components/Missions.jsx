a1bcec332daff079c8f1fdab791c48d8ce36722b
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-04-06 19:32:15 -0300

    adiciona um pouco de css

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Title from './Title';
 import Mission from '../data/missions';
 import MissionCard from './MissionCard';
+import '../App.css';
 
 class Missions extends React.Component {
   render() {
--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../App.css';
 
 class PlanetCard extends React.Component {
   render() {
     const { planetName, planetImage } = this.props;
     return (
-      <div data-testid="planet-card">
+      <div data-testid="planet-card" className="planet-box">
         <p data-testid="planet-name">{ planetName }</p>
         <img src={ planetImage } alt={ `Planeta ${planetName}` } />
       </div>
